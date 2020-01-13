7292af231bf683638da1f63b271e6cc79e465eb2
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-01-14 00:38:03 +0200

    feature: sort array bottom up recurring num test

--- test/SortArrayBottomUp.java
+++ test/SortArrayBottomUp.java
@@ -43,6 +43,20 @@ public class SortArrayBottomUp {
     }
 
     @Test
+    public void sortArrayRecurringNum() {
+        try {
+            int[] arr = {5,2,8,5,5,1};
+            int[] sortedArr = {1,2,5,5,5,8};
+            int[] sortedAns = Program.sortArray(arr);
+            for (int i = 0; i < arr.length; i++) {
+                assertEquals(sortedAns[i], sortedArr[i]);
+            }
+        } catch (Exception e) {
+            fail("should not throw error");
+        }
+    }
+
+    @Test
     public void sortArrayWithMinus() {
         try {
             int[] arr = {3,-2,-8,5,6,1};
