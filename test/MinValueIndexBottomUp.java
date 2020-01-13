cc51f8d8110310f13034b0986617496e13ae19c5
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-01-13 23:22:20 +0200

    feature: min value index bottom up

---
+++ test/MinValueIndexBottomUp.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class MinValueIndexBottomUp {
+
+    @Test
+    public void minValueIndexNULL() {
+        try {
+            int minValueIndex = Program.minValueIndex(null);
+            assertEquals(-1, minValueIndex);
+        } catch (Exception e) {
+            fail("should not throw error");
+        }
+    }
+
+    @Test
+    public void minValueIndexEmpty() {
+        try {
+            int[] arr = new int[0];
+            int minValueIndex = Program.minValueIndex(arr);
+            assertEquals(-1, minValueIndex);
+        } catch (Exception e) {
+            fail("should not throw error");
+        }
+    }
+
+    @Test
+    public void minValueIndex() {
+        int[] arr = {3,4,5,2,7,3};
+        int minValueIndex = Program.minValueIndex(arr);
+        assertEquals(3, minValueIndex);
+    }
+}
